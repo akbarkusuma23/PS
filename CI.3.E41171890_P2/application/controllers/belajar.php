dd4b41ec40331726fbcda4fd73ac72d861be1715
mdkaromi <mdkaromi@users.noreply.github.com>
2019-03-18 16:37:47 +0700

    P2

---
+++ CI.3.E41171890_P2/application/controllers/belajar.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Belajar extends CI_Controller {
+	
+	function __construct(){
+        parent::__construct();
+        $this->load->helper('html');
+		
+	}
+ 
+	public function index(){
+		echo "ini method index pada controller belajar | cara membuat controller pada codeigniter | MD Karomi";
+	}
+ 
+	public function halo(){				
+		$data = array(
+			'judul' => "Cara Membuat View Pada CodeIgniter",
+			'tutorial' => "CodeIgniter"
+			);
+		$this->load->view('view_belajar', $data);
+	}
+ 
+}
